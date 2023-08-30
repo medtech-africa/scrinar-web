37aa0b701cb98d1505a0660b23cc9a83b0b648cc
Ayanrinde damilola david <dammyayans560@gmail.com>
2023-08-30 09:08:54 +0100

    feat: Typography comp.

---
+++ src/stories/Typography..stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Typography } from "./Typography";
+
+const meta = {
+  title: "Example/Typography",
+  component: Typography,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    // children: "Text",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  //   tags: ["autodocs"],
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Page: Story = {};
