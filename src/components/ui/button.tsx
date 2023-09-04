68265f4972da411cdca9553ce20e8b2a15fdb502
asahwells <asahwells@gmail.com>
2023-09-04 14:07:23 +0100

    fix:added compoundVariant for the dsabled

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,33 +5,51 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-lg text-sm font-medium transition-colors',
   {
     variants: {
       variant: {
-        default:
-          'bg-grey-800 text-grey-50 hover:bg-grey-900 focus:border-2 border-grey-200',
-        primary:
-          'bg-lust-800 text-grey-50 hover:bg-lust-900 focus:border-2 border-lust-200 disabled:bg-lust-50 disabled:text-lust-200 ',
+        default: 'bg-grey-800 text-grey-50 hover:bg-grey-900 border-grey-200',
+        primary: 'bg-lust-800 text-grey-50 hover:bg-lust-900 border-lust-200',
         secondary:
-          'bg-secondary text-grey-900 hover:bg-sunglow-800 focus:border-2 border-sunglow-200',
-        disabledPrimary: 'bg-lust-50 text-lust-200 ',
-        disabledSecondary: 'bg-sunglow-100 text-gray-400 ',
-        disabledDefault: 'bg-gray-100 text-gray-400 ',
+          'bg-secondary text-grey-900 hover:bg-sunglow-800 border-sunglow-200',
         focused: 'border-2 border-lust-200 bg-primary text-primary-foreground',
       },
+
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'rounded-lg px-4 py-[7px]',
-        md: 'rounded-lg px-4 py-2',
-        lg: 'rounded-lg px-4 py-3',
-        xl: 'rounded-lg px-4 py-4',
+        sm: 'px-4 py-[7px]',
+        md: 'px-4 py-2',
+        lg: 'px-4 py-3',
+        xl: 'px-4 py-4',
         // icon: "h-10 w-10",
       },
+      disabled: {
+        true: 'pointer-events-none',
+        false: 'focus:border-2 cursor-pointer ',
+      },
     },
+    compoundVariants: [
+      {
+        variant: 'primary',
+        disabled: true,
+        class: 'bg-lust-50 text-lust-200',
+      },
+      {
+        variant: 'secondary',
+        disabled: true,
+        class: 'bg-sunglow-100 text-gray-400 ',
+      },
+      {
+        variant: 'default',
+        disabled: true,
+        class: 'bg-gray-100 text-gray-400',
+      },
+    ],
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      disabled: false,
     },
   }
 )
@@ -40,17 +58,26 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  disabled?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    { className, variant, size, asChild = false, formAction: _, ...props },
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      disabled,
+      formAction: _,
+      ...props
+    },
     ref
   ) => {
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className, disabled }))}
         ref={ref}
         {...props}
       />
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -11,6 +11,11 @@ const meta = {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
   args: {
     children: 'Button',
   },
@@ -22,6 +27,12 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    children: 'Button',
+  },
+}
 export const Primary: Story = {
   args: {
     variant: 'primary',
