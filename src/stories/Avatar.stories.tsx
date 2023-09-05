f471b9c031fc6f099ab4083fd321f4b7541ec114
Benjamin Oni <onibenjo@gmail.com>
2023-09-05 23:38:02 +0100

    chore: avatar

---
+++ src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar'
+
+const meta = {
+  title: 'Example/Avatar',
+  component: Avatar,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta
+
+export default meta
+
+export const Example = () => (
+  <Avatar>
+    <AvatarImage src="https://img.lovepik.com/element/40128/7461.png_1200.png" />
+    <AvatarFallback>CN</AvatarFallback>
+  </Avatar>
+)
