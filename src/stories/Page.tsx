29bd57fe993b04807c7c703c024248ba9ff0062a
Asah Victor <52835749+asahwells@users.noreply.github.com>
2023-09-08 09:46:03 +0100

    feat: header (#16)
    * feat: header
    
    * fix: removed unnecessary codes
    
    * fix:header component
    
    * fix: changed to icon-picker & moved header
    
    ---------
    
    Co-authored-by: Ayanrinde damilola david <dammyayans560@gmail.com>

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 
-import { Header } from './Header'
+import { Header } from '../components/ui/Header'
 import './page.css'
 
-type User = {
-  name: string
-}
-
 export const Page: React.FC = () => {
-  const [user, setUser] = React.useState<User>()
-
   return (
     <article>
-      <Header
-        user={user}
-        onLogin={() => setUser({ name: 'Jane Doe' })}
-        onLogout={() => setUser(undefined)}
-        onCreateAccount={() => setUser({ name: 'Jane Doe' })}
-      />
+      <Header />
 
       <section className="storybook-page">
         <h2>Pages in Storybook</h2>
