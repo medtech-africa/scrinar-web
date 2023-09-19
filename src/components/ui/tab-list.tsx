25afc0959bb4342af63266a3c21755c54998eaf7
Ayanrinde damilola david <52835978+dammyayans@users.noreply.github.com>
2023-09-19 05:45:49 -0500

    feat: added tablist (#33)

---
+++ src/components/ui/tab-list.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@/lib/utils'
+import React, { FC } from 'react'
+import { Text } from './text'
+
+export type ITabList = {
+  onClickTabItem: (label: string) => void
+  activeTab: string
+  labels: string[]
+}
+
+export const TabList: FC<ITabList> = ({
+  labels,
+  onClickTabItem,
+  activeTab,
+}) => {
+  const isActive = (label: string) =>
+    label === (activeTab ? activeTab : labels?.[0])
+  const onClick = (label: string) => () => onClickTabItem(label)
+
+  return (
+    <section className="flex flex-wrap">
+      {labels.map((label) => (
+        <Text
+          as="span"
+          className={cn(
+            'text-sm text-grey-700 py-2.2 px-4 transition-all',
+            isActive(label) &&
+              'border-b-2 border-primary font-bold text-grey-900'
+          )}
+          key={label}
+          onClick={onClick(label)}
+        >
+          {label}
+        </Text>
+      ))}
+    </section>
+  )
+}
