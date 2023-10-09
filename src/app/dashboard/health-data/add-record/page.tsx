5426236212450c5668294eeba81263c767adfaa4
Ayanrinde damilola david <52835978+dammyayans@users.noreply.github.com>
2023-10-09 07:33:36 -0500

    feat: dashboard quick link (#58)

--- src/app/dashboard/health-data/add-record/page.tsx
+++ src/app/dashboard/health-data/add-record/page.tsx
@@ -57,7 +57,7 @@ type IDataToSend = {
   physicalActivityScore?: string
 }
 
-export default function AddRecord() {
+export const AddHealthDataRecordContent = () => {
   const [level, setLevel] = useState<SelectVal | null>()
   const { data: studentsData, isLoading: studentsLoading } = useStudents()
   const [student, setStudent] = useState<Student | null>()
@@ -154,6 +154,236 @@ export default function AddRecord() {
       },
     })
   }
+
+  return (
+    <div className="w-full h-full">
+      <PageCard title="Student Bio Data" bodyStyle="p-4">
+        <div className="flex items-end">
+          <div className="flex items-center">
+            {student ? (
+              <Avatar
+                src={student?.avatarUrl}
+                fallback={returnJoinedFirstCharacter(
+                  student?.firstName,
+                  student?.lastName
+                )}
+              />
+            ) : (
+              <div className="p-4 rounded-full border border-lust-100 border-dashed ">
+                <IconPicker icon="add" className="text-lust-900" />
+              </div>
+            )}
+            <Text
+              className="ml-2 text-gray-900"
+              variant="text/md"
+              weight="medium"
+            >
+              Student Avatar
+            </Text>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+          <Select
+            label="Class"
+            full
+            labelStyle="lg:text-sm text-xs"
+            placeholder="Select Class"
+            options={schoolLevels}
+            onChange={(val) => setLevel(val as SelectVal)}
+            value={level}
+          />
+
+          <Select
+            label="Student"
+            full
+            labelStyle="lg:text-sm text-xs"
+            placeholder="Select Student"
+            isLoading={studentsLoading}
+            isDisabled={!level}
+            options={students}
+            onChange={(val) => setStudent(val as Student)}
+            value={student}
+          />
+          {student && (
+            <>
+              <Input
+                label="Student Age"
+                disabled
+                defaultValue={student?.age}
+                labelStyle="lg:text-sm text-xs"
+              />
+              <Input
+                label="Student Gender"
+                disabled
+                defaultValue={student?.gender}
+                labelStyle="lg:text-sm text-xs"
+                className="capitalize"
+              />
+            </>
+          )}
+        </div>
+      </PageCard>
+
+      <div className="grid md:grid-cols-2 gap-6 py-7 mt-2">
+        <PageCard title="Antropometry">
+          <div className="flex gap-3 w-full p-4">
+            <Input
+              placeholder="0"
+              label="Height(cm)"
+              labelStyle="flex justify-center items-center"
+              value={height}
+              onChange={(e) => setHeight(e.target.value)}
+              variant={variantValidityCheck(height)}
+              message={messageCheck(height)}
+            />
+            <Input
+              placeholder="0"
+              label="Weight(kg)"
+              labelStyle="flex justify-center items-center"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+              variant={variantValidityCheck(weight)}
+              message={messageCheck(weight)}
+            />
+            <Input
+              placeholder="0"
+              label="Waist(cm)"
+              labelStyle="flex justify-center items-center"
+              value={waist}
+              onChange={(e) => setWaist(e.target.value)}
+              variant={variantValidityCheck(waist)}
+              message={messageCheck(waist)}
+            />
+          </div>
+          <div className="mt-4">
+            <div className="bg-grey-50 w-full p-4 flex justify-center">
+              <Text>BMI Result</Text>
+            </div>
+            <div className="flex flex-col justify-center items-center gap-y-4 p-4">
+              <Text
+                variant="display/sm"
+                weight="bold"
+                className="text-grey-700"
+              >
+                {bmi}
+              </Text>
+              {!!bmi && (
+                <BadgeField
+                  variant={calculateBmiRisk(bmi)?.variant}
+                  value={calculateBmiRisk(bmi)?.message}
+                />
+              )}
+            </div>
+            <Label className="px-4 flex justify-center">
+              * BMI automatically generated
+            </Label>
+          </div>
+        </PageCard>
+
+        <div className="h-full">
+          <PageCard title="Blood Pressure" bodyStyle="p-4">
+            <div className="grid grid-cols-[2fr_1fr] items-center">
+              <div className="flex">
+                <Input
+                  placeholder="000"
+                  label="Sys"
+                  labelStyle="lg:text-sm text-xs"
+                  value={sys}
+                  onChange={(e) => setSys(e.target.value)}
+                  variant={variantValidityCheck(sys)}
+                  message={messageCheck(sys)}
+                />
+                <Text className="mt-6 mx-2" variant="display/sm">
+                  /
+                </Text>
+                <Input
+                  placeholder="00"
+                  label="Dys"
+                  labelStyle="lg:text-sm text-xs"
+                  value={dys}
+                  onChange={(e) => setDys(e.target.value)}
+                  variant={variantValidityCheck(dys)}
+                  message={messageCheck(dys)}
+                />
+              </div>
+              {sys && dys && (
+                <BadgeField
+                  variant={
+                    calculateBloodPressureRisk(Number(sys), Number(dys)).variant
+                  }
+                  value={
+                    calculateBloodPressureRisk(Number(sys), Number(dys)).message
+                  }
+                  className="ml-2 mt-6"
+                />
+              )}
+            </div>
+          </PageCard>
+
+          <PageCard title="Blood Sugar" bodyStyle="p-4 mt-4">
+            <div className="grid grid-cols-[2fr_1fr]  items-center">
+              <Input
+                placeholder="170"
+                label="RBS (mg/dL)"
+                labelStyle="lg:text-sm text-xs"
+                onChange={(e) => setBloodSugar(e.target.value)}
+                variant={variantValidityCheck(bloodSugar)}
+                message={messageCheck(bloodSugar)}
+              />
+              {bloodSugar && (
+                <BadgeField
+                  variant={
+                    categorizeBloodSugarLevel(Number(bloodSugar)).variant
+                  }
+                  value={categorizeBloodSugarLevel(Number(bloodSugar)).message}
+                  className="ml-2 mt-6"
+                />
+              )}
+            </div>
+          </PageCard>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2 gap-6 py-7 mt-2">
+        <PageCard title="Nutritional Assess" bodyStyle="p-4">
+          <div className="flex gap-3 items-center">
+            <Label>Dietary Diversity Score - 0/15</Label>
+            <BadgeField variant="error" value="Poor" />
+            <Text
+              variant="text/sm"
+              className="text-primary cursor-pointer underline"
+              as="span"
+            >
+              Open Questionaire
+            </Text>
+          </div>
+        </PageCard>
+        <PageCard title="Exercise/Activity" bodyStyle="p-4">
+          <div className="flex gap-3 items-center">
+            <Label>Phyical Activity Score - 0/15</Label>
+            <BadgeField variant="error" value="Poor" />
+            <Text
+              variant="text/sm"
+              className="text-primary cursor-pointer underline"
+              as="span"
+            >
+              Open Questionaire
+            </Text>
+          </div>
+        </PageCard>
+      </div>
+      <Button
+        variant={'primary'}
+        value="Save Data"
+        leadingIcon={<IconPicker icon="saveAdd" />}
+        className="mt-6"
+        onClick={handleSubmit}
+        loading={isLoading}
+      />
+    </div>
+  )
+}
+
+export default function AddRecord() {
   return (
     <div>
       <PageHeader
@@ -162,234 +392,7 @@ export default function AddRecord() {
         navigation={navigationItems}
         avatar="avatar"
       />
-      <div className="w-full h-full">
-        <PageCard title="Student Bio Data" bodyStyle="p-4">
-          <div className="flex items-end">
-            <div className="flex items-center">
-              {student ? (
-                <Avatar
-                  src={student?.avatarUrl}
-                  fallback={returnJoinedFirstCharacter(
-                    student?.firstName,
-                    student?.lastName
-                  )}
-                />
-              ) : (
-                <div className="p-4 rounded-full border border-lust-100 border-dashed ">
-                  <IconPicker icon="add" className="text-lust-900" />
-                </div>
-              )}
-              <Text
-                className="ml-2 text-gray-900"
-                variant="text/md"
-                weight="medium"
-              >
-                Student Avatar
-              </Text>
-            </div>
-          </div>
-          <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
-            <Select
-              label="Class"
-              full
-              labelStyle="lg:text-sm text-xs"
-              placeholder="Select Class"
-              options={schoolLevels}
-              onChange={(val) => setLevel(val as SelectVal)}
-              value={level}
-            />
-
-            <Select
-              label="Student"
-              full
-              labelStyle="lg:text-sm text-xs"
-              placeholder="Select Student"
-              isLoading={studentsLoading}
-              isDisabled={!level}
-              options={students}
-              onChange={(val) => setStudent(val as Student)}
-              value={student}
-            />
-            {student && (
-              <>
-                <Input
-                  label="Student Age"
-                  disabled
-                  defaultValue={student?.age}
-                  labelStyle="lg:text-sm text-xs"
-                />
-                <Input
-                  label="Student Gender"
-                  disabled
-                  defaultValue={student?.gender}
-                  labelStyle="lg:text-sm text-xs"
-                  className="capitalize"
-                />
-              </>
-            )}
-          </div>
-        </PageCard>
-
-        <div className="grid md:grid-cols-2 gap-6 py-7 mt-2">
-          <PageCard title="Antropometry">
-            <div className="flex gap-3 w-full p-4">
-              <Input
-                placeholder="0"
-                label="Height(cm)"
-                labelStyle="flex justify-center items-center"
-                value={height}
-                onChange={(e) => setHeight(e.target.value)}
-                variant={variantValidityCheck(height)}
-                message={messageCheck(height)}
-              />
-              <Input
-                placeholder="0"
-                label="Weight(kg)"
-                labelStyle="flex justify-center items-center"
-                value={weight}
-                onChange={(e) => setWeight(e.target.value)}
-                variant={variantValidityCheck(weight)}
-                message={messageCheck(weight)}
-              />
-              <Input
-                placeholder="0"
-                label="Waist(cm)"
-                labelStyle="flex justify-center items-center"
-                value={waist}
-                onChange={(e) => setWaist(e.target.value)}
-                variant={variantValidityCheck(waist)}
-                message={messageCheck(waist)}
-              />
-            </div>
-            <div className="mt-4">
-              <div className="bg-grey-50 w-full p-4 flex justify-center">
-                <Text>BMI Result</Text>
-              </div>
-              <div className="flex flex-col justify-center items-center gap-y-4 p-4">
-                <Text
-                  variant="display/sm"
-                  weight="bold"
-                  className="text-grey-700"
-                >
-                  {bmi}
-                </Text>
-                {!!bmi && (
-                  <BadgeField
-                    variant={calculateBmiRisk(bmi)?.variant}
-                    value={calculateBmiRisk(bmi)?.message}
-                  />
-                )}
-              </div>
-              <Label className="px-4 flex justify-center">
-                * BMI automatically generated
-              </Label>
-            </div>
-          </PageCard>
-
-          <div className="h-full">
-            <PageCard title="Blood Pressure" bodyStyle="p-4">
-              <div className="grid grid-cols-[2fr_1fr] items-center">
-                <div className="flex">
-                  <Input
-                    placeholder="000"
-                    label="Sys"
-                    labelStyle="lg:text-sm text-xs"
-                    value={sys}
-                    onChange={(e) => setSys(e.target.value)}
-                    variant={variantValidityCheck(sys)}
-                    message={messageCheck(sys)}
-                  />
-                  <Text className="mt-6 mx-2" variant="display/sm">
-                    /
-                  </Text>
-                  <Input
-                    placeholder="00"
-                    label="Dys"
-                    labelStyle="lg:text-sm text-xs"
-                    value={dys}
-                    onChange={(e) => setDys(e.target.value)}
-                    variant={variantValidityCheck(dys)}
-                    message={messageCheck(dys)}
-                  />
-                </div>
-                {sys && dys && (
-                  <BadgeField
-                    variant={
-                      calculateBloodPressureRisk(Number(sys), Number(dys))
-                        .variant
-                    }
-                    value={
-                      calculateBloodPressureRisk(Number(sys), Number(dys))
-                        .message
-                    }
-                    className="ml-2 mt-6"
-                  />
-                )}
-              </div>
-            </PageCard>
-
-            <PageCard title="Blood Sugar" bodyStyle="p-4 mt-4">
-              <div className="grid grid-cols-[2fr_1fr]  items-center">
-                <Input
-                  placeholder="170"
-                  label="RBS (mg/dL)"
-                  labelStyle="lg:text-sm text-xs"
-                  onChange={(e) => setBloodSugar(e.target.value)}
-                  variant={variantValidityCheck(bloodSugar)}
-                  message={messageCheck(bloodSugar)}
-                />
-                {bloodSugar && (
-                  <BadgeField
-                    variant={
-                      categorizeBloodSugarLevel(Number(bloodSugar)).variant
-                    }
-                    value={
-                      categorizeBloodSugarLevel(Number(bloodSugar)).message
-                    }
-                    className="ml-2 mt-6"
-                  />
-                )}
-              </div>
-            </PageCard>
-          </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-6 py-7 mt-2">
-          <PageCard title="Nutritional Assess" bodyStyle="p-4">
-            <div className="flex gap-3 items-center">
-              <Label>Dietary Diversity Score - 0/15</Label>
-              <BadgeField variant="error" value="Poor" />
-              <Text
-                variant="text/sm"
-                className="text-primary cursor-pointer underline"
-                as="span"
-              >
-                Open Questionaire
-              </Text>
-            </div>
-          </PageCard>
-          <PageCard title="Exercise/Activity" bodyStyle="p-4">
-            <div className="flex gap-3 items-center">
-              <Label>Phyical Activity Score - 0/15</Label>
-              <BadgeField variant="error" value="Poor" />
-              <Text
-                variant="text/sm"
-                className="text-primary cursor-pointer underline"
-                as="span"
-              >
-                Open Questionaire
-              </Text>
-            </div>
-          </PageCard>
-        </div>
-        <Button
-          variant={'primary'}
-          value="Save Data"
-          leadingIcon={<IconPicker icon="saveAdd" />}
-          className="mt-6"
-          onClick={handleSubmit}
-          loading={isLoading}
-        />
-      </div>
+      <AddHealthDataRecordContent />
     </div>
   )
 }
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,6 +3,9 @@
 import * as React from 'react'
 import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { cn } from '@/lib/utils'
+import { IconPicker } from './icon-picker'
+import { Button } from './button'
+import { Text } from './text'
 
 const Dialog = DialogPrimitive.Root
 
@@ -41,16 +44,24 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full',
+        'fixed md:left-[50%] top-0 bottom-0 md:top-[50%] z-50 grid w-full h-full max-w-[100%] md:max-w-[80%] md:max-h-[90%] md:translate-x-[-50%] md:translate-y-[-50%] gap-4 border bg-white overflow-y-hidden md:p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
         className
       )}
       {...props}
     >
-      {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <span className="text-base">X</span>
-        <span className="sr-only">Close</span>
-      </DialogPrimitive.Close>
+      <div className="overflow-y-auto">{children}</div>
+      {!!props?.title && (
+        <DialogPrimitive.Close className="absolute right-4 top-0 pt-4 left-4 transition-opacity ho focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground bg-white">
+          <div className="flex justify-between mb-2">
+            <Text variant="text/lg">{props.title}</Text>
+            <Button
+              variant="tertiary"
+              onlyIcon={<IconPicker icon="closeSquare" />}
+            />
+          </div>
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
+      )}
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
