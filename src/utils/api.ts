1e7552584d584302097be775542fc657f2122875
Ayanrinde damilola david <52835978+dammyayans@users.noreply.github.com>
2023-10-19 05:32:59 -0500

    feat: Dashboard stats and card link (#67)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -32,6 +32,7 @@ export const API = {
   //  school
   schoolRegister: `${BASE_URL}/api/v1/school`,
   schoolUpdate: `${BASE_URL}/api/v1/school/me`,
+  schoolDashboard: `${BASE_URL}/api/v1/school/dashboard`,
 
   // misc
   getState: `${BASE_URL}/api/v1/misc/states-with-lga`,
