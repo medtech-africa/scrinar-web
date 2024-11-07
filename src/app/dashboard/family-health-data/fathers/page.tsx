31919330dda0b2f97c23a7092e4470ac955881b4
asahwells <asahwells@gmail.com>
2024-11-07 18:29:37 +0100

    feat: added family health data

---
+++ src/app/dashboard/family-health-data/fathers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Fathers = () => {
+  return <div>Fathers</div>
+}
+
+export default Fathers
---
+++ src/app/dashboard/family-health-data/mothers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Mothers = () => {
+  return <div>Mothers</div>
+}
+
+export default Mothers
