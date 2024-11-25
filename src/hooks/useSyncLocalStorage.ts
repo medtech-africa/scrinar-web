88f07298278f0072690cab7b1f2a8ca113a44c99
Benjamin Oni <onibenjo@gmail.com>
2024-11-25 14:36:26 +0100

    chore: sync parent and children local storage

--- src/hooks/useSyncLocalStorage.ts
+++ src/hooks/useSyncLocalStorage.ts
@@ -1,9 +1,9 @@
 import { API } from '@/utils/api'
 import baseAxios from '@/utils/baseAxios'
-import { useLocalParentSurveyStore } from './useLocalParentSurvey'
+import { useLocalParentSurveyStore, useLocalStudentSurveyStore } from './useLocalParentSurvey'
 import { useEffect } from 'react'
 
-export const useSyncLocalStorage = () => {
+export const useSyncParentLocalStorage = () => {
   const allSurvey = useLocalParentSurveyStore((state) => state.data)
 
   const syncForm = (data: { id: string; formData: object }) => {
@@ -28,9 +28,42 @@ export const useSyncLocalStorage = () => {
       // use promise.all settled instead
       Promise.allSettled(allSurveyArray.map((survey) => syncForm(survey))).then(
         (results) => {
-          console.log('results', results)
+          console.log('results', results.map((result) => result.status))
         }
       )
     }
   }, [])
+}
+export const useSyncChildrenLocalStorage = () => {
+  const allSurvey = useLocalStudentSurveyStore((state) => state.data)
+
+  const syncForm = (data: { id: string; formData: object }) => {
+    //   @ts-expect-error createdAt
+    delete data?.formData?.createdAt
+    //   @ts-expect-error updatedAt
+    delete data?.formData?.updatedAt
+    //   @ts-expect-error parent
+    delete data?.formData?.parent
+    //   @ts-expect-error id for the parent
+    delete data?.formData?.id
+
+    return baseAxios.patch(API.studentSurvey(data.id), data.formData)
+  }
+
+  useEffect(() => {
+    if (allSurvey.length > 0) {
+      const allSurveyArray = Object.values(allSurvey)
+      Promise.allSettled(allSurveyArray.map((survey) => syncForm(survey))).then(
+        (results) => {
+          console.log('results', results.map((result) => result.status))
+        }
+      )
+    }
+  }, [])
+}
+export const useSyncLocalStorage = () => {
+  useSyncParentLocalStorage()
+  useSyncChildrenLocalStorage()
+
+  return null
 }
