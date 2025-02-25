6f643da27f906549d1135d2ef55ba97f750543fb
Ayanrinde damilola david <dammyayans560@gmail.com>
2025-02-25 09:03:23 +0100

    chore: clean up

--- src/app/dashboard/projects/new/page.tsx
+++ src/app/dashboard/projects/new/page.tsx
@@ -2,34 +2,12 @@
 
 import { PageHeader } from '@/components/page-header'
 import { IconNames } from '@/components/ui/icon-picker/icon-names'
-// import calculateAge from '@/utils/calculateAge'
 import { AddNewProjectContent } from './add-new-project-content'
 
 const navigationItems = [
-  { label: 'User Profile', icon: IconNames.arrowRight },
   { label: 'Projects', icon: IconNames.arrowRight },
   { label: 'Add New Project' },
 ]
-
-export interface IFormValue {
-  id?: string
-  email?: string
-  firstName: string
-  lastName: string
-  familyCode?: string
-  mobile?: string
-  gender: { value: string; label: string }
-  avatar?: boolean
-  age?: number
-  isGuardian?: boolean
-}
-
-export interface IDataToSend
-  extends Omit<IFormValue, 'level' | 'gender' | 'avatar'> {
-  level: string
-  gender: string
-  // age: number
-}
 
 export default function AddNewProject() {
   return (
