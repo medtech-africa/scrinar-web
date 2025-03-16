bc7d1d63ca8800f6d11f0f01bbe77a9ed1a0c1cb
Benjamin Oni <onibenjo@gmail.com>
2025-03-17 00:02:55 +0100

    open surveillance to all

--- src/app/dashboard/surveillance/page.tsx
+++ src/app/dashboard/surveillance/page.tsx
@@ -28,19 +28,19 @@ const SurveillanceMap = dynamic(
   }
 )
 
-const isValidUser = (roles: string[] = []) => {
-  const validUsers = [
-    'school',
-    'super_admin',
-    'jica_researchers',
-    'organization',
-  ]
+// const isValidUser = (roles: string[] = []) => {
+//   const validUsers = [
+//     'school',
+//     'super_admin',
+//     'jica_researchers',
+//     'organization',
+//   ]
 
-  return roles.every((val) => validUsers.includes(val))
-}
+//   return roles.every((val) => validUsers.includes(val))
+// }
 
 export default function SurveillancePage() {
-  const user = useUser((state) => state.user)
+  // const user = useUser((state) => state.user)
   const loading = useUser((state) => state.loading)
   const { isPending } = useSurveillanceAnalytics()
 
@@ -48,14 +48,14 @@ export default function SurveillancePage() {
     return <ContentLoader loading />
   }
 
-  if (!isValidUser(user?.roles))
-    return (
-      <div className="h-[70vh]">
-        <div className="flex items-center justify-center h-full">
-          <p className="text-2xl font-semibold">Unauthorized Access</p>
-        </div>
-      </div>
-    )
+  // if (!isValidUser(user?.roles))
+  //   return (
+  //     <div className="h-[70vh]">
+  //       <div className="flex items-center justify-center h-full">
+  //         <p className="text-2xl font-semibold">Unauthorized Access</p>
+  //       </div>
+  //     </div>
+  //   )
 
   return (
     <div className="w-full">
